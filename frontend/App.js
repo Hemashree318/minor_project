3e514b17cce1f632b3938463b82c2fefe63485ea
Hemashree <hemashreen60@gmail.com>
2025-09-07 22:54:50 +0530

    changes

--- frontend/App.js
+++ frontend/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello world</Text>
+      <Text>New project</Text>
       <StatusBar style="auto" />
     </View>
   );
